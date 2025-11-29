dc4651bad3e9f2ce59538ec17fd0268c469a8b90
Parsa Hashemkhany <parsahashemkhany.1385.pppp@gmail.com>
2025-11-29 05:21:38 -0800

    faq retriever function updated

--- app/tools/retriever_tool.py
+++ app/tools/retriever_tool.py
@@ -4,9 +4,13 @@ from langchain_core.tools import Tool
 retriever = Retriever()
 
 
-def faq_retriever_func(query):
-    results = retriever.get_retriever(query)
-    return results
+def faq_retriever_func(query: str) -> str:
+    docs = retriever.get_retriever(query)
+
+    if not docs:
+        return "No relevant information found in the FAQs."
+
+    return "\n\n".join([doc.page_content for doc in docs])
 
 
 faq_tool = Tool(
