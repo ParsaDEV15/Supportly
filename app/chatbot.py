27aee077b399580e7f8e0e80ff82255e3ad49e07
Parsa Hashemkhany <parsahashemkhany.1385.pppp@gmail.com>
2025-12-23 01:16:26 -0800

    memory added for agent

--- app/chatbot.py
+++ app/chatbot.py
@@ -1,10 +1,11 @@
 from langchain_openai import ChatOpenAI
 from langchain_classic.agents import create_openai_tools_agent, AgentExecutor
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+from langchain_classic.memory import ConversationBufferWindowMemory
 from app.tools.retriever_tool import faq_tool
 from app.tools.orders_getter_tool import get_order_status_tool
 from app.tools.recommend_product_tool import recommend_product_tool
-from langchain_core.runnables.history import RunnableWithMessageHistory
 from config.settings import LLM_MODEL
 
 
@@ -12,23 +13,41 @@ class ChatBot:
     def __init__(self):
         chat_bot = ChatOpenAI(model=LLM_MODEL)
 
-        system_prompt = open('app/prompts/system_prompt.txt', 'r', encoding='utf-8').read()
+        prompt_text = open('app/prompts/system_prompt.txt', 'r', encoding='utf-8').read()
 
         prompt = ChatPromptTemplate.from_messages([
-            ("system", system_prompt),
+            ("system", prompt_text),
+            ('placeholder', '{chat_history}'),
             ("human", "{input}"),
             ('placeholder', '{agent_scratchpad}')
         ])
 
         tools = [faq_tool, get_order_status_tool, recommend_product_tool]
 
+        chat_history = StreamlitChatMessageHistory(key="supportly_user")
+
+        memory = ConversationBufferWindowMemory(
+            llm=chat_bot,
+            chat_memory=chat_history,
+            return_messages=True,
+            memory_key="chat_history",
+            k=5
+        )
+
         agent = create_openai_tools_agent(
             llm=chat_bot,
             prompt=prompt,
-            tools=tools
+            tools=tools,
         )
 
-        self.executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+        self.executor_with_memory = AgentExecutor(
+            agent=agent,
+            tools=tools,
+            memory=memory,
+            verbose=True,
+        )
 
     def get_response(self, query):
-        return self.executor.invoke({'input': query})
+        return self.executor_with_memory.invoke(
+            {'input': query}
+        )
